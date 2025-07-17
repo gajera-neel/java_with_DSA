4dbc7c5e2b34d75120051f0b9039ec942336f79b
gajera-neel <neelgajera3535@gmail.com>
2025-07-17 18:28:43 +0530

    modification

--- Sum.java
+++ Sum.java
@@ -1,23 +1,24 @@
+
 import java.util.Scanner;
-class Sum{
-    public static void main(String args[]){
+
+class Sum {
+
+    public static void main(String args[]) {
         System.out.println("Enter two numbers to calculate their sum:");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        int even=0;
+        int even = 0;
         int odd = 0;
-        for(int i=0; i<=num; i++){
-            
-            if (i % 2 == 0){
-                even +=i;
+        for (int i = 0; i <= num; i++) {
+
+            if (i % 2 == 0) {
+                even += i;
+            } else {
+                odd += i;
             }
-            else{
-                odd +=i;
-            }
-            
-            
+
         }
-       System.out.println("The sum of even numbers from 1 to " + num + " is: " + even);
-       System.out.println("The sum of odd numbers from 1 to " + num + " is: " + odd);
+        System.out.println("The sum of even numbers from 1 to " + num + " is: " + even);
+        System.out.println("The sum of odd numbers from 1 to " + num + " is: " + odd);
     }
 }
